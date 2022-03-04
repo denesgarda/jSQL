00cb89626ef550a86fe3f57ee2aa61c7d1a4805f
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2022-03-03 19:10:55 -0800

    moved

--- jSQL/test/test.js
+++
@@ -1,7 +0,0 @@
-const jSQL = require("../index");
-
-const database = new jSQL.Database(__dirname + "/database");
-
-const accounts = database.execute(".|'main'|'accounts'|{'username': [ [ 'user1' ], [ true ] ]}");
-
-console.log(accounts)
